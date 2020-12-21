024290c5a1060ad3efe5f0dcb54e23ab16bce55d
Ilan Fleitman <ilan.fleitman@gmail.com>
2020-12-22 00:38:24 +0200

    base react app
    add base of react dashboard app

---
+++ src/components/Main.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import Navbar from "./Navbar";
+import { Switch, Route, Redirect } from 'react-router-dom'
+import Footer from "./Footer";
+import UserProfile from "./UserProfile";
+import Dashboard from "./Dashboard";
+
+class Main extends Component {
+    render() {
+        return <div className="main-panel">
+                <Navbar />
+                <Switch>
+                    <Route path="/dashboard" component={Dashboard} />
+                    <Route path="/profile" component={UserProfile} />
+                    <Redirect from='*' to='/dashboard' />
+                </Switch>
+                <Footer />
+        </div>
+    }
+}
+
+export default Main
